85be0452370e26afcf14e67a52876c92fbcedf63
Vinayaka Patil <vinnu313@gmail.com>
2018-11-30 22:01:19 +0530

    Timer redone.

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -7,58 +7,50 @@ import { CountdownComponent } from 'ngx-countdown';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
-  time = 240;
-  min = '00';
+  show = true;
+  time = 0;
+  ptime = 0;
+  interval: any;
+  min = '05';
   sec = '00';
-  // config;
-  // @ViewChild(CountdownComponent) counter: CountdownComponent;
-  constructor() {
-    //   const vm = this;
-    //   vm.config = {
-    //     leftTime: 10
-    //   };
-    this.init();
-  }
+  loop = true;
+  constructor() { }
 
   init() {
-    setInterval(() => {
-      if (this.time > 0) {
-        if (this.time === 60) {
-          const audio = new Audio('../../assets/airhorn.mp3');
-          audio.play();
-        }
-        this.time -= 1;
-        this.format(this.time);
-      } else {
-        const audio = new Audio('../../assets/alarm.mp3');
-        audio.play();
-        this.time = 240;
-      }
-    }, 1000);
+    clearInterval(this.interval);
+    this.show = false;
+    this.time = this.ptime;
+    this.interval = setInterval(this.tick.bind(this), 1000);
   }
+
+  tick() {
+    if (this.time > 0) {
+      if (this.time === 60) {
+        const audio = new Audio('../../assets/airhorn.mp3');
+        audio.play();
+      }
+      this.time -= 1;
+      this.format(this.time);
+    } else {
+      if (this.loop) {
+        this.time = this.ptime;
+        this.show = false;
+      } else {
+        this.stop();
+      }
+      const audio = new Audio('../../assets/alarm.mp3');
+      audio.play();
+    }
+  }
+
+  stop() {
+    this.show = true;
+    clearInterval(this.interval);
+  }
+
   format(now) {
     this.min = (now / 60 | 0).toString().padStart(2, '0');
     this.sec = (now % 60 | 0).toString().padStart(2, '0');
   }
-  // ngAfterViewInit() {
-  //   const vm = this;
-  //   // vm.counter.begin();
-  // }
-  // start() {
-  //   const vm = this;
-  //   vm.counter.begin();
-  // }
-  // restart() {
-  //   const vm = this;
-  //   window.location.reload();
-  // }
-  // onStart() {
-  //   console.log('Started at ' + Date.now());
-  // }
-  // onFinished() {
-  //   const vm = this;
-  //   vm.counter.restart();
-  //   console.log('Finished at ' + Date.now());
-  // }
 }
 
