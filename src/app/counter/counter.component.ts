7c6bf8391a4846050b4279da8cadd1221ea21eac
vinayakp <vinayakapatil@Vinayakas-MacBook-Pro.local>
2023-12-02 10:30:53 +0530

    Added delay.

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -8,6 +8,9 @@ import { CountdownComponent } from 'ngx-countdown';
 })
 export class CounterComponent {
   show = true;
+  delay = 0;
+  pdelay = 0;
+  isDelay = false;
   time = 0;
   ptime = 0;
   interval: any;
@@ -20,26 +23,38 @@ export class CounterComponent {
     clearInterval(this.interval);
     this.show = false;
     this.time = this.ptime;
+    this.delay = this.pdelay;
+    this.isDelay = false;
     this.interval = setInterval(this.tick.bind(this), 1000);
   }
 
   tick() {
     if (this.time > 0) {
-      if (this.time === 60) {
+      if (this.time === 16) {
         const audio = new Audio('assets/airhorn.mp3');
+        audio.play();
+      }
+      if (this.time === 1) {
+        const audio = new Audio('assets/alarm.mp3');
         audio.play();
       }
       this.time -= 1;
       this.format(this.time);
     } else {
       if (this.loop) {
-        this.time = this.ptime;
-        this.show = false;
+        if (this.delay > 0) {
+          this.isDelay = true;
+          this.delay -= 1;
+          this.format(this.delay);
+        } else {
+          this.time = this.ptime;
+          this.delay = this.pdelay;
+          this.isDelay = false;
+          this.show = false;
+        }
       } else {
         this.stop();
       }
-      const audio = new Audio('assets/alarm.mp3');
-      audio.play();
     }
   }
 
